3dd29ce8ee62569574688c987a14e5fc8c3dd881
gabrielmxavier <gabrielxavierbonano@gmail.com>
2020-04-24 17:44:13 -0300

    Last Update

--- app.js
+++ app.js
@@ -291,13 +291,13 @@ var UIController = (function() {
             var now = new Date();
 
             months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-            //dates = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', '31st', '32nd'];
+            dates = [' ', '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', '31st', '32nd'];
 
             month = now.getMonth();
             day = now.getDate();
             year = now.getFullYear();
-            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
-            //document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + dates[day] + ''+ ', ' + year;
+            //document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + dates[day] + ''+ ', ' + year;
 
         },
 
